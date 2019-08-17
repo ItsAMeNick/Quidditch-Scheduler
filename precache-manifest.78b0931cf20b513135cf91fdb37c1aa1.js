db918bb01ddd9b8504f4a3f8d0366a1c31054955
ItsAMeNick <zonern@rpi.edu>
2019-08-17 01:16:54 -0400

    Updates

---
+++ precache-manifest.78b0931cf20b513135cf91fdb37c1aa1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c40943957705c27c3e5e651a58c5015",
+    "url": "/Quidditch-Scheduler/index.html"
+  },
+  {
+    "revision": "6a45e2caa028b2248e27",
+    "url": "/Quidditch-Scheduler/static/js/2.48237d71.chunk.js"
+  },
+  {
+    "revision": "ebb17efbf96a739bb309",
+    "url": "/Quidditch-Scheduler/static/js/main.241f0eae.chunk.js"
+  },
+  {
+    "revision": "37742c3b96301ada23e0",
+    "url": "/Quidditch-Scheduler/static/js/runtime~main.6df91746.js"
+  }
+]);
